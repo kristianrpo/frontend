1b2676a58d45249ea7aca94d62e6ed2654ba6bd0
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-10-30 00:33:03 -0500

    feat: add new network elements

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -17,8 +17,18 @@ data "aws_route_table" "main" {
   }
 }
 
-# Agregar ruta a Internet Gateway
+# Verificar si la ruta ya existe
+locals {
+  has_internet_route = length([
+    for route in data.aws_route_table.main.routes :
+    route if route.cidr_block == "0.0.0.0/0"
+  ]) > 0
+}
+
+# Agregar ruta a Internet Gateway solo si no existe
 resource "aws_route" "internet_access" {
+  count = local.has_internet_route ? 0 : 1
+
   route_table_id         = data.aws_route_table.main.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
